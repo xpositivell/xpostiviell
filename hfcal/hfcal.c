6c254f998d3af9ae9fbb5ce1c09c9349b0a25ef5
xpositivell <saibert.nikita2021@mail.ru>
2022-12-05 04:43:30 +0000

    hfcal

---
+++ hfcal/hfcal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "hfcal.h"
+
+void display_calories(float weight, float distance, float coeff)
+{
+    printf("Вес: %3.2f кг\n", weight);
+    printf("Расстояние: %3.2f фунты\n", distance);
+    printf("Сожжено калорий: %4.2f кал\n", coeff * weight * distance);
+}
---
+++ hfcal/hfcal.h
@@ -0,0 +1,6 @@
+#ifndef __HFCAL_H
+#define __HFCAL_H
+
+void display_calories(float, float, float);
+
+#endif
